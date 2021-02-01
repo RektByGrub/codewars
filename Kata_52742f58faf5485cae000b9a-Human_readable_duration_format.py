35ca58fba81e3375c212af2d025c4aaccf5130b3
RektByGrub <78346822+RektByGrub@users.noreply.github.com>
2021-02-01 12:10:07 +0000

    Add files via upload

---
+++ Kata_52742f58faf5485cae000b9a-Human_readable_duration_format.py
@@ -0,0 +1,45 @@
+def format_duration(seconds):
+
+    ## If the value is 0
+    if seconds == 0:
+        return "now"
+
+    ## Create empty counters
+    return_list = [
+        ["year", seconds // 31536000],
+        ["day", seconds % 31536000 // 86400],
+        ["hour", seconds % 31536000 % 86400 // 3600],
+        ["minute", seconds % 31536000 % 86400 % 3600 // 60],
+        ["second", seconds % 31536000 % 86400 % 3600 % 60 // 1]
+    ]
+
+    ## Remove empties
+    clean_list = []
+    for item in return_list:
+        if item[1] != 0:
+            clean_list.append(item)
+
+    ## Assemble return
+    try:
+        ## Last item
+        if clean_list[-1][1] == 1:
+            return_string = f"{str(clean_list[-1][1])} {str(clean_list[-1][0])}"
+        else:
+            return_string = f"{str(clean_list[-1][1])} {str(clean_list[-1][0])}s"
+
+        ## Second to last item
+        if clean_list[-2][1] == 1:
+            return_string = f"{str(clean_list[-2][1])} {str(clean_list[-2][0])} and {return_string}"
+        else:
+            return_string = f"{str(clean_list[-2][1])} {str(clean_list[-2][0])}s and {return_string}"
+
+        ## Every other
+        for item in reversed(clean_list[:-2]):
+            if item[1] == 1:
+                return_string = f"{str(item[1])} {str(item[0])}, {return_string}"
+            else:
+                return_string = f"{str(item[1])} {str(item[0])}s, {return_string}"
+    except:
+        pass
+
+    return return_string
